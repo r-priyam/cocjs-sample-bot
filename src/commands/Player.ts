4714182647516a45864a4184154e5d384e7c7e01
Priyam <priyamroy29@gmail.com>
2022-03-19 23:38:37 +0530

    chore: finish player command

--- src/commands/Player.ts
+++ src/commands/Player.ts
@@ -18,8 +18,23 @@ export = {
 
         try {
             await interaction.deferReply();
-            const player = await interaction.client.coc.getPlayer('#2PP');
-            const embed = new MessageEmbed().setTitle(`${player.name}`).setThumbnail(player.league.icon.url);
+            const player = await interaction.client.coc.getPlayer(playerTag!);
+            const embed = new MessageEmbed()
+                .setTitle(`${player.name}`)
+                .setThumbnail(player.league.icon.url)
+                // https://clashofclans.js.org/docs/api/interfaces/APIPlayer
+                .addFields(
+                    { name: 'Exp. Level', value: `${player.expLevel}`, inline: false },
+                    { name: 'Town Hall', value: `${player.townHallLevel}`, inline: false },
+                    { name: 'War Stars', value: `${player.warStars}`, inline: false },
+                    { name: 'Trophies', value: `${player.trophies}`, inline: false },
+                    { name: 'Donations', value: `${player.donations}`, inline: false },
+                    { name: 'Donations Received', value: `${player.received}`, inline: false },
+                    { name: 'Attcks/Defense', value: `${player.attackWins}/${player.defenseWins}`, inline: false }
+                )
+                .setColor([0xe74c3c, 0x2980b9, 0x1abc9c, 0xe67e22, 0xf1c40f][Math.floor(Math.random() * 6)])
+                .setURL(`https://link.clashofclans.com/en?action=OpenPlayerProfile&tag=${player.tag.replace(/#/g, '')}`)
+                .setTimestamp();
             await interaction.editReply({ embeds: [embed] });
         } catch (error) {
             if (error instanceof HTTPError && error.message === 'notFound') {
